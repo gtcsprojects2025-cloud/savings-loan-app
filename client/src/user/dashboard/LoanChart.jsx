47e85d170d4d0ebf4d40a0cd422185727c6a388c
RolandMario <rolandmario2@gmail.com>
2026-02-20 07:22:27 +0100

    extracting phone No2

--- client/src/user/dashboard/LoanChart.jsx
+++ client/src/user/dashboard/LoanChart.jsx
@@ -149,7 +149,7 @@ const LoanChart = () => {
       console.warn('No email found in localStorage');
       setError('Email is required');
     }
-  }, );
+  },[email, fetchTransactions] );
 
   useEffect(() => {
     console.log('LoanChart final data:', data);
