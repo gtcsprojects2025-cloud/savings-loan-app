b31c07b4d3dd3886d6d4d6cc6f82988b2f9b9528
RolandMario <rolandmario2@gmail.com>
2026-02-22 11:13:33 +0100

    formatted phone No 2

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -161,13 +161,14 @@ Date: ${date}`;
             //  const userRegistered = await Register.findOne({BVN: req.body.BVN})
             //  await sendSMSNG(`+${userRegistered.phoneNo}`, smsBody)
             if (!depositAccount) return res.status(404).json({message:'User not found'});
-            res.status(200).json({ message: 'Deposit successfully!' });
+            
             const transaction_details = new TRANSACTION(account_details);
             await transaction_details.save();
             //  await transporter.sendMail(mailOptions);
             // await sendMail(req.body.email, 'Deposit Transaction', 'Deposit', emailBody)
             console.log('formatted phone No: ',formatNigerianNumber(userRegistered.phoneNo))
             await sendSMSNG(formatNigerianNumber(userRegistered.phoneNo), smsBody)
+            res.status(200).json({ message: 'Deposit successfully!' });
         }
         }else if(req.body.transactionType==="withdraw"){
             // withdraw logic
